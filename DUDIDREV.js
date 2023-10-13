9403ecc7075dd985a6838fb6baa9d54649d4e47e
Wepado <147792821+Wepado@users.noreply.github.com>
2023-10-13 10:16:33 +0500

    Add files via upload

---
+++ DUDIDREV.js
@@ -0,0 +1,49 @@
+// let nomer=-12;
+// if(nomer<0){
+//     console.log('отрицала')
+// } else {
+//     console.log('не отрицала')
+// }
+
+// let strochka = "я участница беремена в 16 задавайте вопросы";
+// console.log (strochka.length)
+
+// let strochka="я участница беремена в 16 задавайте вопросы";
+// let strochka2= strochka[strochka.length -1];
+// console.log(strochka2); 
+
+// let str = "афк";
+// let h = str[str.length -1];
+// console.log(h);
+
+// let n = 10
+// if (n % 2 === 0) {
+// console.log("чет")
+// }
+// else{
+//     console.log('нечет')
+// }
+
+// const w1 = 'Я русский';
+// const w2 = 'Я';
+// if (w1.charAt(0)===w2.charAt(0)){
+//     console.log('совпадают')
+// } else {
+// console.log('не совпадают')
+// }
+
+// let n= 828;
+// let str=n.toString();
+// let p= parseInt(str[0]);
+// let s= parseInt(str[str.length -1]);
+// let k = p + s;
+// console.log(k);
+
+// let n = 1337;
+// let s = 0;
+
+// while(n > 0){
+//     s += n % 10
+//     n = Math.floor(n/10);
+// }
+// console.log(s)
